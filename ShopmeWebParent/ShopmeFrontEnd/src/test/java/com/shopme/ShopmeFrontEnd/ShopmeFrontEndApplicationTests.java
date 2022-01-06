14dce34c7b2701ba93e71c28445d0d00e12df00a
Lucian Trepteanu <lucian.trepteanu@gmail.com>
2022-01-06 20:09:49 +0200

    Setup Modules

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/shopme/ShopmeFrontEnd/ShopmeFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopme.ShopmeFrontEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmeFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
